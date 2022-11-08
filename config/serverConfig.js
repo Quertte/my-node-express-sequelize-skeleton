294a7aede69b54b98b568e736aa2597be7fbdbed
Quertte <a.injen90@gmail.com>
2022-11-08 15:38:44 +0200

    init db

---
+++ config/serverConfig.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
+
+const serverConfig = (app) => {
+  app.use(morgan('tiny', { stream: accessLogStream }));
+};
