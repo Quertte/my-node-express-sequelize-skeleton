fab3f766f7963b7d29559db83a1b244dfc571c25
Quertte <a.injen90@gmail.com>
2022-11-08 16:01:14 +0200

    configuration server

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 require('dotenv').config();
-
 require('@babel/register');
+
+const express = require('express');
+const { sequelize } = require('./db/models');
+const serverConfig = require('./config/serverConfig');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+
+serverConfig(app);
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT} port`);
+  try {
+    sequelize.authenticate();
+    console.log('Соединение с БД установлено');
+  } catch (error) {
+    console.log('Ошибка при подключении к бд', error.message);
+  }
+});
--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,8 +1,22 @@
+const morgan = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const fs = require('fs');
 const path = require('path');
+const sessionConfig = require('./sessionConfig');
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
 
+// главная конфигурация приложения
 const serverConfig = (app) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(morgan('tiny', { stream: accessLogStream }));
+  app.use(express.static('public'));
+  app.use(cookieParser());
+  app.use(session(sessionConfig));
 };
+
+module.exports = serverConfig;
